66dddd8c7aaa241f91e511a080aeebb677de2f41
yoavi2 <yoav.codish@gmail.com>
2014-06-14 20:29:07 +0300

    Stage1

---
+++ MapTarget/src/com/example/maptarget/MainActivity.java
@@ -0,0 +1,290 @@
+package com.example.maptarget;
+
+import java.util.ArrayList;
+
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.GooglePlayServicesUtil;
+import com.google.android.gms.location.LocationListener;
+import com.google.android.gms.maps.CameraUpdate;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.LatLngBounds;
+import com.google.android.gms.maps.model.Marker;
+import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.maps.MapFragment;
+import com.javadocmd.simplelatlng.LatLngTool;
+import com.javadocmd.simplelatlng.util.LengthUnit;
+
+import android.app.Activity;
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.location.GpsStatus;
+import android.location.Location;
+import android.location.LocationManager;
+import android.os.Bundle;
+import android.provider.Settings;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
+
+public class MainActivity extends Activity {
+
+	private GoogleMap mMap;
+	private LocationManager mLocationService;
+	private ArrayList<Marker> mMarkers;
+	private boolean mLocationSet = false;
+	private Dialog mGpsDialog;
+	private final static int SET_TARGET = 3007;
+
+	static public enum target_type {
+		FRIEND, ENEMY
+	}
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+
+		this.mLocationService = (LocationManager) getSystemService(LOCATION_SERVICE);
+
+		//  Build gps alert
+		AlertDialog.Builder builder = new AlertDialog.Builder(this);
+		builder.setTitle("Location Services Not Active");
+		builder.setMessage("Please enable Location Services and GPS");
+		builder.setPositiveButton("OK",
+				new DialogInterface.OnClickListener() {
+					@Override
+					public void onClick(DialogInterface dialogInterface,
+							int i) {
+						// Show location settings when the user acknowledges
+						// the alert dialog
+						Intent intent = new Intent(
+								Settings.ACTION_LOCATION_SOURCE_SETTINGS);
+						startActivity(intent);
+					}
+				});
+		builder.setNegativeButton("Exit",
+				new DialogInterface.OnClickListener() {
+					@Override
+					public void onClick(DialogInterface dialogInterface,
+							int i) {
+						// Exit Application
+						finish();
+					}
+				});
+		this.mGpsDialog = builder.create();
+		this.mGpsDialog.setCanceledOnTouchOutside(false);
+		
+		// Check if enabled and if not send user to the GPS settings
+		if (!this.mLocationService
+				.isProviderEnabled(LocationManager.GPS_PROVIDER)) {
+			this.mGpsDialog.show();
+		}
+		// Getting Google Play availability status
+		int status = GooglePlayServicesUtil
+				.isGooglePlayServicesAvailable(getBaseContext());
+
+		// Showing status
+		if (status != ConnectionResult.SUCCESS) { // Google Play Services
+													// are
+													// not available
+
+			int requestCode = 10;
+			Dialog dialog = GooglePlayServicesUtil.getErrorDialog(status, this,
+					requestCode);
+			dialog.show();
+
+		} else { // Google Play Services are available
+
+			this.mMap = ((MapFragment) getFragmentManager().findFragmentById(
+					R.id.map)).getMap();
+			this.mMap.setMapType(GoogleMap.MAP_TYPE_SATELLITE);
+
+			this.mMarkers = new ArrayList<Marker>();
+
+			this.mLocationService.requestLocationUpdates(LocationManager.GPS_PROVIDER,0,0,new android.location.LocationListener() {
+				@Override
+				public void onProviderEnabled(String provider) {
+					if (mGpsDialog.isShowing()) {
+						mGpsDialog.dismiss();
+					}
+				}
+				@Override
+				public void onProviderDisabled(String provider) {
+					if (!mLocationSet) {
+						mGpsDialog.show();
+					}
+				}
+				@Override
+				public void onLocationChanged(Location location) {
+				}
+				@Override
+				public void onStatusChanged(String provider, int status,
+						Bundle extras) {
+				}
+			});
+
+			GoogleMap.OnMyLocationChangeListener locationChangeListener = new GoogleMap.OnMyLocationChangeListener() {
+				@Override
+				public void onMyLocationChange(Location location) {
+
+					if (!mLocationSet) {
+
+						adjustMap();
+						mLocationSet = true;
+						invalidateOptionsMenu();
+					}
+				}
+			};
+			this.mMap.setOnMyLocationChangeListener(locationChangeListener);
+
+			// Enabling MyLocation Layer of Google Map
+			this.mMap.setMyLocationEnabled(true);
+
+		}
+	}
+
+	@Override
+	protected void onRestart() { 
+
+		// Check if location set and if enabled and if not send user to the GPS
+		// settings
+		if (this.mLocationSet) {
+			this.adjustMap();
+		} else if (!this.mLocationService
+				.isProviderEnabled(LocationManager.GPS_PROVIDER)
+				&& !this.mGpsDialog.isShowing()) {
+
+			this.mGpsDialog.show();
+		}
+
+		super.onRestart();
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.main, menu);
+		return true;
+	}
+
+	@Override
+	public boolean onPrepareOptionsMenu(Menu menu) {
+		menu.getItem(0).setEnabled(this.mLocationSet);
+		return super.onPrepareOptionsMenu(menu);
+	}
+
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		switch (item.getItemId()) {
+		case R.id.action_set_target:
+			this.showSetTarget();
+			return true;
+		case R.id.action_settings:
+			return true;
+		default:
+			return super.onOptionsItemSelected(item);
+		}
+
+	}
+
+	private void showSetTarget() {
+		Intent intent = new Intent(this, SetTargetActivity.class);
+		startActivityForResult(intent, SET_TARGET);
+	}
+
+	@Override
+	public void onActivityResult(int requestCode, int resultCode, Intent intent) {
+		super.onActivityResult(requestCode, resultCode, intent);
+
+		if (resultCode == Activity.RESULT_OK) {
+			switch (requestCode) {
+			case (SET_TARGET):
+				if (!this.mLocationSet) {
+					Toast.makeText(this, "Location is not yet set by GPS",
+							Toast.LENGTH_LONG).show();
+				} else {
+
+					Bundle bundle = intent.getExtras();
+					String name = bundle
+							.getString(SetTargetActivity.EXTRA_NAME);
+					int azimuth = bundle
+							.getInt(SetTargetActivity.EXTRA_AZIMUTH);
+					int distance = bundle
+							.getInt(SetTargetActivity.EXTRA_DISTANCE_IN_METERS);
+					target_type type = (target_type) bundle
+							.getSerializable(SetTargetActivity.EXTRA_TARGET_TYPE);
+
+//					Location currloc = this.mLocationService // TODO: FIX BUG ` IF GPS DISABLED
+//							.getLastKnownLocation(LocationManager.GPS_PROVIDER);
+					Location currloc = this.mMap.getMyLocation();
+
+					com.javadocmd.simplelatlng.LatLng currPoint = new com.javadocmd.simplelatlng.LatLng(
+							currloc.getLatitude(), currloc.getLongitude());
+
+					com.javadocmd.simplelatlng.LatLng destPointLib = LatLngTool
+							.travel(currPoint, azimuth, distance,
+									LengthUnit.METER);
+
+					LatLng destPointGoogle = new LatLng(
+							destPointLib.getLatitude(),
+							destPointLib.getLongitude());
+
+					Marker destMark = this.mMap.addMarker(new MarkerOptions()
+							.position(destPointGoogle).title(name)
+							.snippet(type.toString()));
+
+					if (type == MainActivity.target_type.FRIEND) {
+						destMark.setIcon(BitmapDescriptorFactory
+								.fromResource(R.drawable.friend));
+					} else {
+						destMark.setIcon(BitmapDescriptorFactory
+								.fromResource(R.drawable.enemy));
+					}
+
+					this.mMarkers.add(destMark);
+
+					this.adjustMap();
+
+					break;
+				}
+			}
+		}
+	}
+
+	private void adjustMap() {
+
+		Location location = this.mLocationService
+				.getLastKnownLocation(LocationManager.GPS_PROVIDER);
+		
+		if (location == null) {
+			location = this.mMap.getMyLocation();
+		}
+		
+		LatLng loc = new LatLng(location.getLatitude(), location.getLongitude());
+		mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(loc, 16.0f));
+
+		if (this.mMarkers.size() > 0) {
+
+			LatLngBounds.Builder builder = new LatLngBounds.Builder();
+			builder.include(loc);
+
+			for (Marker marker : this.mMarkers) {
+				builder.include(marker.getPosition());
+			}
+
+			CameraUpdate cu = CameraUpdateFactory.newLatLngBounds(
+					builder.build(),
+					getResources().getDisplayMetrics().widthPixels,
+					getResources().getDisplayMetrics().heightPixels,
+					getResources().getDisplayMetrics().heightPixels / 6);
+			this.mMap.animateCamera(cu);
+
+		}
+	}
+
+}
---
+++ MapTarget/src/com/example/maptarget/SetTargetActivity.java
@@ -0,0 +1,139 @@
+package com.example.maptarget;
+
+import android.support.v7.app.ActionBarActivity;
+import android.app.Activity;
+import android.content.Intent;
+import android.graphics.drawable.Drawable;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.EditText;
+import android.widget.NumberPicker;
+import android.widget.RadioGroup;
+import android.widget.Spinner;
+import android.widget.Toast;
+
+public class SetTargetActivity extends ActionBarActivity {
+
+	public final static String EXTRA_NAME = "com.example.maptarget.name";
+	public final static String EXTRA_AZIMUTH = "com.example.maptarget.azimuth";
+	public final static String EXTRA_DISTANCE_IN_METERS = "com.example.maptarget_distance";
+	public final static String EXTRA_TARGET_TYPE = "com.example.maptarget.target_type";
+	private EditText mETName;
+	private NumberPicker mNPAzimuth;
+	private EditText mETDistance;
+	private Spinner mSpinnerDistanceUnit;
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_set_target);
+
+		this.mETName = (EditText) findViewById(R.id.et_name);
+		
+		this.mNPAzimuth = (NumberPicker) findViewById(R.id.np_azimuth);
+		this.mNPAzimuth.setMinValue(0);
+		this.mNPAzimuth.setMaxValue(359);
+		this.mNPAzimuth.setOnLongPressUpdateInterval(10);
+
+		this.mETDistance = (EditText) findViewById(R.id.et_distance);
+
+		this.mSpinnerDistanceUnit = (Spinner) findViewById(R.id.spinner_distance_type);
+		// Create an ArrayAdapter using the string array and a default spinner
+		// layout
+		ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(
+				this, R.array.distance_unit_array,
+				android.R.layout.simple_spinner_item);
+		// Specify the layout to use when the list of choices appears
+		adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+		// Apply the adapter to the spinner
+		this.mSpinnerDistanceUnit.setAdapter(adapter);
+
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.set_target, menu);
+		return true;
+	}
+
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		// Handle action bar item clicks here. The action bar will
+		// automatically handle clicks on the Home/Up button, so long
+		// as you specify a parent activity in AndroidManifest.xml.
+		int id = item.getItemId();
+		if (id == R.id.action_settings) {
+			return true;
+		}
+		return super.onOptionsItemSelected(item);
+	}
+
+	public void accept(View view) {
+
+		// Validation
+		if (this.mETName.getText().toString().trim().isEmpty()){
+			
+Toast.makeText(this, "Name field is mandatory!", Toast.LENGTH_LONG).show();
+			
+			// Draw pencil
+			Drawable pencil = getResources().getDrawable(R.drawable.pencil2);
+			pencil.setBounds(0, 0, pencil.getIntrinsicWidth(), pencil.getIntrinsicHeight());
+			this.mETName.setCompoundDrawables(null, null, pencil, null);
+			
+		}
+	else if (this.mETDistance.getText().toString().isEmpty()) {
+			
+			Toast.makeText(this, "Distance field is mandatory!", Toast.LENGTH_LONG).show();
+			
+			// Draw pencil
+			Drawable pencil = getResources().getDrawable(R.drawable.pencil2);
+			pencil.setBounds(0, 0, pencil.getIntrinsicWidth(), pencil.getIntrinsicHeight());
+			this.mETDistance.setCompoundDrawables(null, null, pencil, null);
+			
+		} else {
+
+			Intent intent = new Intent(this, MainActivity.class);
+
+			intent.putExtra(EXTRA_NAME, this.mETName.getText().toString());
+			
+			intent.putExtra(EXTRA_AZIMUTH, this.mNPAzimuth.getValue());
+
+			int distanceInMeters = Integer
+					.parseInt(((EditText) findViewById(R.id.et_distance))
+							.getText().toString());
+			if (this.mSpinnerDistanceUnit.getSelectedItem().toString()
+					.equals(getString(R.string.text_kilometer))) { 
+				distanceInMeters *= 1000;
+			}
+			intent.putExtra(EXTRA_DISTANCE_IN_METERS, distanceInMeters);
+
+			MainActivity.target_type tt_type = MainActivity.target_type.FRIEND;
+			if (((RadioGroup) findViewById(R.id.rg_target_type))
+					.getCheckedRadioButtonId() == R.id.rb_enemy) {
+				tt_type = MainActivity.target_type.ENEMY;
+			}
+			intent.putExtra(EXTRA_TARGET_TYPE, tt_type);
+
+			setResult(Activity.RESULT_OK, intent);
+			finish();
+		}
+	}
+
+	public void cancel(View view) {
+		finish();
+	}
+	
+public void onDistanceFieldClick(View view){
+	this.mETDistance.setCompoundDrawables(null, null, null, null);
+}
+
+public void onNameFieldClick(View view){
+	this.mETName.setCompoundDrawables(null, null, null, null);
+}
+
+}
